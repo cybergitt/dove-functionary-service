ec48a4b85c87013a79d6fa96642d85b5089ffa4a
Sigit Prayitno <cybergitt@gmail.com>
2025-11-23 22:29:15 +0700

    Add spring boot starter

---
+++ src/main/java/com/sinodash/getdovenote/api/functionary_service/FunctionaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sinodash.getdovenote.api.functionary_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionaryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinodash/getdovenote/api/functionary_service/FunctionaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinodash.getdovenote.api.functionary_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
